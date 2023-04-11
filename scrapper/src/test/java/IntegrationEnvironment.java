f81120c1f5577d271f4ada561a7e0605f1f0ff18
Renat Gainutdinov <renatgh@gmail.com>
2023-04-11 21:50:59 +0300

    Task 5, config.properties added

--- scrapper/src/test/java/IntegrationEnvironment.java
+++ scrapper/src/test/java/IntegrationEnvironment.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 import liquibase.Contexts;
 import liquibase.LabelExpression;
@@ -17,21 +20,38 @@ import org.testcontainers.junit.jupiter.Container;
 
 public abstract class IntegrationEnvironment {
 
+    static final Properties PROPERTIES = new Properties();
+
+    // load configuration
+    static {
+        try (InputStream input = IntegrationEnvironment.class.getClassLoader().getResourceAsStream("config.properties")) {
+            PROPERTIES.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    static final String MIGRATION_DIR = PROPERTIES.getProperty("migrationDir");
+    static final String MIGRATION_FILE = PROPERTIES.getProperty("migrationFile");
+    static final String DATABASE_IMAGE = PROPERTIES.getProperty("testsContainer.databaseImage");
+    static final String DATABASE_NAME = PROPERTIES.getProperty("testsContainer.databaseName");
+    static final String USERNAME = PROPERTIES.getProperty("testsContainer.username");
+    static final String PASSWORD = PROPERTIES.getProperty("testsContainer.password");
+
+
     @Container
     static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
-    static final String MIGRATION_DIR = "migrations";
-    static final String MIGRATION_XML = "master.xml";
-
     static {
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer("postgres:14.7")
-                .withDatabaseName("integration-tests-db")
-                .withUsername("username")
-                .withPassword("password");
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer(DATABASE_IMAGE)
+                .withDatabaseName(DATABASE_NAME)
+                .withUsername(USERNAME)
+                .withPassword(PASSWORD);
         POSTGRESQL_CONTAINER.start();
 
+        // create path to migration XML file
         Path migrationsPath = new File(".").toPath().toAbsolutePath().getParent().getParent().resolve(MIGRATION_DIR);
-        String changelogPath = MIGRATION_XML;
+        String changelogPath = MIGRATION_FILE;
 
         try (Connection connection = DriverManager.getConnection(
                 POSTGRESQL_CONTAINER.getJdbcUrl(),
